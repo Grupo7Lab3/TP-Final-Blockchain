48f1a5c73ca28db9a15a879c08064e0e91cb42c0
Ariel <ArielHernandez171@gmail.com>
2022-05-25 18:46:05 -0300

    Funciones añadidas a clase wallet

--- src/com/company/Wallet.java
+++ src/com/company/Wallet.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Wallet {
     private int amount;
     private String CodeSecurity;
     private int Trasfer;
 
-    public Wallet(int amount, String codeSecurity, int trasfer) {
-        this.amount = amount;
+    public Wallet(String codeSecurity, int trasfer) {
+        this.amount = 100;
         this.CodeSecurity = codeSecurity;
         this.Trasfer = trasfer;
     }
@@ -28,12 +31,49 @@ public class Wallet {
         this.CodeSecurity = codeSecurity;
     }
 
-    public int getTrasfer() {
+    /*public int getTrasfer() {
         return this.Trasfer;
     }
 
     public void setTrasfer(int trasfer) {
-        Trasfer = trasfer;
+        this.Trasfer = trasfer;
+    }*/
+
+    /*public void checkTranser(ArrayList<Transfer> transferArray, User user){
+        int index=0;
+        while (transferArray.size()<index){
+            if (transferArray.get(index).getTransfer()!=this.Trasfer&&transferArray.get(index).getStatus()==Status.Pendiente){
+                transferArray.get(index).setValidated(user);
+            }
+            index++;
+        }
+    }*///no estoy seguro si deberia estar aca
+    public Transfer newtransfer(String codeSecurity){
+        int amount=0;
+        Scanner scanInt=new Scanner(System.in);
+        do{
+            System.out.println("\nIngrese cuanto quiere transferir:");
+            amount=scanInt.nextInt();
+            if (amount>this.amount){
+                System.out.println("\nEl monto ingresado es mayor a lo que tiene en la wallet");
+            }
+        }while(amount<this.amount);
+        System.out.println("La transferencia pasa a validarse");//Lo pense como que el int trasfer es un codigo para rastrear transferencias
+        Transfer trans=new Transfer(this.Trasfer,codeSecurity,this.CodeSecurity,amount);
+        return trans;
     }
+    public ArrayList<String> getTransferenciasValidadas(ArrayList<Transfer> transfers){
+        ArrayList<String> aux=new ArrayList<>();
+        int contador=transfers.size();
+        while(contador<transfers.size()){
+            if(transfers.get(contador).getTransfer()==this.Trasfer&&transfers.get(contador).getStatus()==Status.Validado){
+                aux.add(transfers.get(contador).toString());
+            }
+            contador++;
+        }
+        return aux;
+    }
+
+
 
 }
