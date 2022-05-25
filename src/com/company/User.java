5e00f5f313546cde03f9fde4db09ba9e8b6ee25d
Lucho <lucianonovello2007@hotmail.com>
2022-05-25 20:14:10 -0300

    Refactor:order project

---
+++ src/com/company/User.java
@@ -0,0 +1,46 @@
+package src.com.company;
+
+public class User {
+        private String mail;
+        private String pass;
+        private String codeSecurity;
+
+        public String getMail() {
+            return mail;
+        }
+
+        public void setMail(String mail) {
+            this.mail = mail;
+        }
+
+        public String getPass() {
+            return pass;
+        }
+
+        public void setPass(String pass) {
+            this.pass = pass;
+        }
+
+        public String getCodeSecurity() {
+            return codeSecurity;
+        }
+
+        public void setCodeSecurity(String codeSecurity) {
+            this.codeSecurity = codeSecurity;
+        }
+        public double checkBalance(String mail, String codeSecurity){
+            double test= 0;
+            return test;
+        }
+
+        public void viewPending(String codeSecurity, String status){
+
+
+        }
+
+        public void validate(){
+
+        }
+
+    }
+
