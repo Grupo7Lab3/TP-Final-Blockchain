df366a01918f534aa6f33c92b7136955935d44e8
Lucho <lucianonovello2007@hotmail.com>
2022-06-16 20:07:37 -0300

    Refactor: Changes in wallet functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,6 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Wallet wallet = new Wallet();
-        List<Wallet> listaWallets = new ArrayList<>();
-        listaWallets.add(wallet);
-
-        FilesJson file = new FilesJson();
-        file.writeToJson("nodes.json", listaWallets);
 
         Login login = new Login();
         login.loggin();
