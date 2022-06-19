f79a24ff6d6899dfe0c96ae28a4c5c28b16718e8
Lucho <lucianonovello2007@hotmail.com>
2022-06-18 23:06:02 -0300

    refactor: changes with transfer validation

--- src/com/company/Wallet.java
+++ src/com/company/Wallet.java
@@ -37,56 +37,65 @@ public class Wallet {
         this.CodeSecurity = codeSecurity;
     }
 
-    public Transfer newTransfer(Scanner scanner, UUID codeSecurity,int idTransfer){
-        double amountTransfer=0;
+    public Transfer newTransfer(Scanner scanner, UUID codeSecurity, int idTransfer) {
+        double amountTransfer = 0;
         LocalDate today = LocalDate.now();
-        if (this.amount == 0){
+        if (this.amount == 0) {
             System.out.println("Saldo insuficiente. ");
             return null;
         }
-        do{
+        do {
             System.out.println("\nIngrese cuanto quiere transferir:");
             amountTransfer = scanner.nextDouble();
-            if (amountTransfer>this.amount){
+            if (amountTransfer > this.amount) {
                 System.out.println("\nEl monto ingresado es mayor a lo que tiene en la wallet");
             }
-        }while(amountTransfer > this.amount);
+        } while (amountTransfer > this.amount);
         System.out.println("La transferencia pasa a validarse");//Lo pense como que el int trasfer es un codigo para rastrear transferencias
         this.amount = this.amount - amountTransfer;
-        Transfer trans=new Transfer(idTransfer,this.CodeSecurity,codeSecurity,amount,today.format(DateTimeFormatter
+        Transfer trans = new Transfer(idTransfer, this.CodeSecurity, codeSecurity, amount, today.format(DateTimeFormatter
                 .ofLocalizedDate(FormatStyle.LONG)));
         System.out.println(trans);
         return trans;
     }
 
 
-    public ArrayList<String> getTransferenciasValidadas(List<Transfer> transfers){
-        ArrayList<String> aux=new ArrayList<>();
-        int contador=transfers.size();
-        while(contador<transfers.size()){
-            if(transfers.get(contador).getStatus().getId() == 1 && transfers.get(contador).getCodeSecurityIn() == this.CodeSecurity){
+    public ArrayList<String> getTransferenciasValidadas(List<Transfer> transfers) {
+        ArrayList<String> aux = new ArrayList<>();
+        int contador = transfers.size();
+        while (contador < transfers.size()) {
+            if (transfers.get(contador).getStatus().getId() == 1 && transfers.get(contador).getCodeSecurityIn() == this.CodeSecurity) {
                 aux.add(transfers.get(contador).toString());
             }
             contador++;
         }
         return aux;
     }
-    public List<String> getTransferenciasNoValidadas(List<Transfer> transfers){
-        List<String> aux=new ArrayList<>();
-        int contador=transfers.size()-1;
-        while(contador!= 0){
-            if(transfers.get(contador).getStatus().getId() == 3 && transfers.get(contador).getCodeSecurityIn() != this.CodeSecurity && this.CodeSecurity != transfers.get(contador).getCodeSecurityOut()){
+
+    public List<String> getTransferenciasNoValidadas(List<Transfer> transfers) {
+        List<String> aux = new ArrayList<>();
+        int contador = transfers.size() - 1;
+        while (contador != 0) {
+            if (transfers.get(contador).getStatus().getId() == 3 && transfers.get(contador).getCodeSecurityIn() != this.CodeSecurity && this.CodeSecurity != transfers.get(contador).getCodeSecurityOut()) {
                 aux.add(transfers.get(contador).toString());//guardarlo en un ArrayList o mostrarlo directamente??
             }
             contador--;
         }
         return aux;
     }
-    public void ValidarTransferencia(List<Transfer> transfers,int id, User user){
-        if(transfers.get(id).getStatus().getId() == 1 || transfers.get(id).getCodeSecurityOut().equals(user.getCodeSecurity()) || transfers.get(id).getCodeSecurityIn().equals(user.getCodeSecurity()))
-            System.out.println("No se puede validar esta transferencia");
-        else
-            transfers.get(id).setValidated(user);
-    }
 
+    public void ValidarTransferencia(List<Transfer> transfers, int id, User user) {
+        for (int i = 0; i < transfers.size(); i++) {
+            if (i == id-1) {
+                if (transfers.get(i).getStatus().getId() == 1 || transfers.get(i).getCodeSecurityOut().equals(user.getCodeSecurity()) || transfers.get(i).getCodeSecurityIn().equals(user.getCodeSecurity()))
+                    System.out.println("No se puede validar esta transferencia");
+                else {
+                    System.out.println(transfers.get(i));
+                    transfers.get(i).setValidated(user);
+                }
+            }
+
+        }
+
+    }
 }
